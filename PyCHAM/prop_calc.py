7a71c169303ce4e37d4b5157c0e7f0b339fac0bc
Simon O'Meara <simon.omeara@manchester.ac.uk>
2023-05-18 13:11:21 +0100

    bypass old umansysprop
    correct import pybel in umansysprop

--- PyCHAM/prop_calc.py
+++ PyCHAM/prop_calc.py
@@ -96,7 +96,16 @@ def prop_calc(rel_SMILES, Pybel_objects, H2Oi, num_comp, Psat_water, vol_Comp,
 		
 		git_url = 'https://github.com/loftytopping/UManSysProp_public.git'
 		Repo.clone_from(git_url, (cwd + '/umansysprop'))
-	
+		
+		# now check on whether import pybel written in data/__init__.py
+		f_init = open(str(cwd + '/umansysprop/data/__init__.py'), mode='r')
+		lines = f.readlines()
+		if 'import pybel' in lines[21]:
+			lines[21] = 'import openbabel.pybel as pybel\n'	
+			f_init = open(str(cwd + '/umansysprop/data/__init__.py'), mode='w')
+			f_init.writelines(lines)
+		f_init.close()
+
 	# point to umansysprop folder
 	sys.path.insert(1, (cwd + '/umansysprop')) # address for updated version
 	
